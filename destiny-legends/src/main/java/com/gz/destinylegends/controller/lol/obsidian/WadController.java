159e1576ad66b0e3321c212070f3b3040db201c0
Destiny_Xue <1369521908@qq.com>
2019-10-23 17:53:21 +0800

    调试,解析wad文件

---
+++ destiny-legends/src/main/java/com/gz/destinylegends/controller/lol/obsidian/WadController.java
@@ -0,0 +1,26 @@
+package com.gz.destinylegends.controller.lol.obsidian;
+
+import com.gz.destinylegends.feign.ObsidianFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Author Destiny_Xue
+ * @Date 2019/10/23 17:44
+ * @Description
+ */
+@RestController
+@RequestMapping("lol/obsidian/wad")
+public class WadController {
+
+    @Autowired
+    ObsidianFeign obsidianFeign;
+
+    @PostMapping("convert")
+    public Object convert(MultipartFile file) {
+        return obsidianFeign.convert(file);
+    }
+}
